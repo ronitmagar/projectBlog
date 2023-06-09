f27dde68a0be2f2e8586cf4e4dede8fe5a16f631
ronit.magar <ronit.magar@datametica.com>
2023-06-10 01:04:04 +0530

    category and post addition

--- blogProject/blog/admin.py
+++ blogProject/blog/admin.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Category, Post
 from django.contrib import admin
 
 # Register your models here.
+
+
+class categoryAdmin(admin.ModelAdmin):
+    list_display = ("image_display" ,"title", "url", "description", "add_date")
+    search_fields = ('title',)
+
+class postAdmin(admin.ModelAdmin):
+    list_display = ("image_display", 'title')
+    search_fields = ('title',)
+    list_filter = ('cat',)
+    list_per_page = 50
+
+
+admin.site.register(Category, categoryAdmin)
+admin.site.register(Post, postAdmin)
--- blogProject/blog/models.py
+++ blogProject/blog/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.html import format_html
 
 # Create your models here.
 
@@ -11,4 +12,28 @@ class Category(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField()
     url = models.CharField(max_length = 100)
+    image = models.ImageField(upload_to = 'category/')
+    add_date = models.DateTimeField(auto_now_add = True, null = True)
+
+    def image_display(self):
+        return format_html('<img src = "/media/{}" style="width:40px;height:40px;border-radius:50%"/>'.format(self.image))
+
+    def __str__(self):
+        return self.title
+
+
+
+class Post(models.Model):
+    post_id = models.AutoField(primary_key = True)
+    title = models.CharField(max_length = 200)
+    content = models.TextField()
+    url = models.CharField(max_length = 100)
+    cat = models.ForeignKey(Category, on_delete = models.CASCADE)
+    image = models.ImageField(upload_to = 'post/')
     
+
+    def image_display(self):
+        return format_html('<img src = "/media/{}" style="width:40px;height:40px;border-radius:50%"/>'.format(self.image))
+    
+    def __str__(self):
+        return self.title
