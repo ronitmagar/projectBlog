dc7a2a69f349ec22b7faed2de179b74064c8e5d8
ronit.magar <ronit.magar@datametica.com>
2023-06-09 20:47:30 +0530

    models addition1

--- blogProject/blog/models.py
+++ blogProject/blog/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    Category_id = models.AutoField(primary_key = True)
+    title = models.CharField(max_length = 100)
+    description = models.TextField()
+    url = models.CharField(max_length = 100)
+    
