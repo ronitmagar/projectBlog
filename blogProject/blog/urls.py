4582493698c567f85c34fbf777035aa5d85e5a9f
ronit.magar <ronit.magar@datametica.com>
2023-07-28 01:51:57 +0530

    frontend start

--- blogProject/blog/urls.py
+++ blogProject/blog/urls.py
@@ -18,10 +18,11 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from blog import views
 
 urlpatterns = [
-    url(r'^admin', admin.site.urls),
-    url(r'', include('blog.urls')),
-    url(r'tinymce/', include('tinymce.urls')),
+    url(r'home', views.home),
+    url(r'^blog/(?P<url>[-\w]+)/$', views.post),
+     
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- blogProject/blog/views.py
+++ blogProject/blog/views.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
+
+
+def home(request):
+    posts = Post.objects.all()[:10]
+    data = {
+        'posts' : posts
+    }
+    return render(request, 'home.html', data)
+
+def post(request, url):
+    # posts = Post.objects.get(url = url)
+    data = {
+        'url' : url
+    }
+    return render(request, 'post.html', data)
